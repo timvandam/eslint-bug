dc6d19735d42937a047daff61d26ae222fe88312
Tim van Dam <timvandamcs@gmail.com>
2021-12-28 20:32:14 +0100

    improve example

--- src/bug.ts
+++ src/bug.ts
@@ -1,12 +1,16 @@
-let x: number;
+function something(): number {
+	let x: number;
 
-function dangerousFunction(): number {
-	if (Math.random() > 0.5) throw new Error('Boom')
-	return 123
-}
+	function dangerousFunction(): number {
+		if (Math.random() > 0.5) throw new Error('Boom')
+		return 123
+	}
 
-try {
-	x = dangerousFunction()
-} catch (e) {
-	//
+	try {
+		x = dangerousFunction() // ESLint: 'x' is never reassigned. Use 'const' instead.(prefer-const)
+	} catch (e) {
+		return -1
+	}
+
+	return x * 20 + 5
 }
